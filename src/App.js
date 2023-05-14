aa27e01fd80d0f5c6421987e314f374aac72c3b8
Kannolas <vanya.pokorsky@gmail.com>
2023-05-14 17:13:14 +0300

    added brasket

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Main from './pages/Main';
 import Catalog from './pages/Catalog'
 import NotFound from './pages/NotFound';
+import Basket from './pages/Basket';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' index element={<Main/>}/>
           <Route path='/catalog' element={<Catalog/>}/>
+          <Route path='/basket' element={<Basket/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>  
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-
-
+import { useState } from "react";
 
 const Nav = function(){
-    
+
+const [dropDown, setDropdown] = useState(false);
+const toggleDropDown =()=>{
+    setDropdown(!dropDown);
+}
 
 const ButtonClicked = function(e){
     let a = [document.querySelector(".about"), document.querySelector(".roadmap"), document.querySelector(".minting"), document.querySelector(".catalog")]
@@ -31,13 +34,22 @@ return(
         
 
         <div className="nav-buttons2">
-            <a href="#"><img className="nav-button1" src="images/PA.png" alt="" /></a>
-            <a href="#"><img className="nav-button1" src="images/bracket.png" alt="" /></a>
-            <a href="#"><img className="nav-button1" src="images/insta.svg" alt="" /></a>
-            <a href="#"><img className="nav-button1" src="images/telegram.svg" alt="" /></a>
+            <img className="nav-button1" onClick={toggleDropDown}  src="images/PA.png" alt="" />
+            <NavLink to={"/basket"}><img className="nav-button1" src="images/bracket.png" alt="" /></NavLink>
+            <a href="https://www.instagram.com" target="_blank"><img className="nav-button1" src="images/insta.svg" alt="" /></a>
+            <a href="https://t.me/Kannolas" target="_blank"><img className="nav-button1" src="images/telegram.svg" alt="" /></a>
 
         </div>
+        
     </div>
+
+    {dropDown && (
+        <div className="dropDown-menu">
+            <div className="dropDown-account">Account</div>
+            <div className="Log-in">Log in</div>
+            
+        </div>
+    )}
     <div className="nav-line"></div>
 </div>
 
